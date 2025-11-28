a534d38ce95db8db89afdb1f8c3fde4cef57a804
Ashams Vs <ashamsvsunil@gmail.com>
2025-11-28 10:49:01 +0530

    Refactor greet and add farewell function

--- demo.py
+++ demo.py
@@ -1,2 +1,8 @@
 def greet(name):
-    return "Hello, " + name + "!"
+    """Return a greeting with punctuation and uppercase."""
+    message = "HELLO, " + name.upper() + "!"
+    return message
+
+def farewell(name):
+    """Say goodbye to the user."""
+    return "Goodbye, " + name
